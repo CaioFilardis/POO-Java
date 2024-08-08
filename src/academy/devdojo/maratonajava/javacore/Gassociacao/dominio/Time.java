49d0ccf5a9e6a1d75233bcefd5aa75d460f8df38
Caio Filardis <caiosantosshake@hotmail.com>
2024-08-07 21:41:54 -0300

    Associação entre objetos Java

---
+++ src/academy/devdojo/maratonajava/javacore/Gassociacao/dominio/Time.java
@@ -0,0 +1,17 @@
+package academy.devdojo.maratonajava.javacore.Gassociacao.dominio;
+
+public class Time {
+    private String name;
+
+    public Time(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
